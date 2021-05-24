6d4237503989797d59bcf92b5eb641517051a17c
gom3s <gmotyl@gmail.com>
2021-05-24 14:13:35 +0200

    feat(domain): base value object class added

--- OrareProMe/Domain/Entity.cs
+++ OrareProMe/Domain/Entity.cs
@@ -17,5 +17,47 @@ namespace OrareProMe.Domain
             Id = id;
             ExternalId = new Guid();
         }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as Entity;
+
+            if (ReferenceEquals(other, null))
+                return false;
+
+            if (ReferenceEquals(this, other))
+                return true;
+
+            if (GetType() != other.GetType())
+                return false;
+
+            if (Id == 0 || other.Id == 0)
+                return false;
+
+            return obj is Entity entity &&
+                   Id == entity.Id &&
+                   ExternalId.Equals(entity.ExternalId);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id, ExternalId);
+        }
+
+        public static bool operator ==(Entity a, Entity b)
+        {
+            if (ReferenceEquals(a, null) && ReferenceEquals(b, null))
+                return true;
+
+            if (ReferenceEquals(a, null) || ReferenceEquals(b, null))
+                return false;
+
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(Entity a, Entity b)
+        {
+            return !(a == b);
+        }
     }
 }
---
+++ OrareProMe/Domain/ValueObject.cs
@@ -0,0 +1,40 @@
+namespace OrareProMe.Domain
+{
+    public abstract class ValueObject<T>
+        where T : ValueObject<T>
+    {
+        protected abstract bool EqualsCore(T other);
+        protected abstract int GetHashCodeCore();
+
+        public override bool Equals(object obj)
+        {
+            var valueObject = obj as T;
+
+            if (ReferenceEquals(valueObject, null))
+                return false;
+
+            return EqualsCore(valueObject);
+        }
+
+        public override int GetHashCode()
+        {
+            return GetHashCodeCore();
+        }
+
+        public static bool operator ==(Entity a, Entity b)
+        {
+            if (ReferenceEquals(a, null) && ReferenceEquals(b, null))
+                return true;
+
+            if (ReferenceEquals(a, null) || ReferenceEquals(b, null))
+                return false;
+
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(Entity a, Entity b)
+        {
+            return !(a == b);
+        }
+    }
+}
